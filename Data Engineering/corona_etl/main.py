6ea6f8f391982bcd268078105dc60c640f76bec6
cyless-hj <jhj6740@gmail.com>
2022-09-20 22:30:08 +0900

    Corona ETL

--- Data Engineering/corona_etl/main.py
+++ Data Engineering/corona_etl/main.py
@@ -1,18 +1,42 @@
-#!/usr/bin/env python3
-"""
-Module Docstring
-"""
+import sys
+from datajob.datamart.co_popu_density import CoPopuDensity
+from datajob.datamart.co_vaccine import CoVaccine
+from datajob.etl.extract.corona_api import CoronaApiExtractor
+from datajob.etl.extract.corona_vaccine import CoronaVaccineExtractor
+from datajob.etl.transform.corona_patient import CoronaPatientTransformer
+from datajob.etl.transform.corona_vaccine import CoronaVaccineTransformer
 
-__author__ = "Your Name"
-__version__ = "0.1.0"
-__license__ = "MIT"
+works = {
+    'extract':{
+        'corona_api': CoronaApiExtractor.extract_data
+        ,'corona_vaccine':CoronaVaccineExtractor.extract_data
+    }
+    ,'transform':{
+        'corona_patient':CoronaPatientTransformer.transform
+        ,'corona_vaccine':CoronaVaccineTransformer.transform
+    }
+    ,'datamart':{
+        'co_popu_density':CoPopuDensity.save
+        ,'co_vaccine':CoVaccine.save
+    }
 
-
-def main():
-    """ Main entry point of the app """
-    print("hello world")
-
+}
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
+    args = sys.argv
+    print(args)
+
+    # main.py 작업(extract, transform, datamart) 저장할 위치(테이블)
+    # 매개변수 2개
+    if len(args) != 3:
+        raise Exception('2개의 전달인자가 필요합니다.')
+
+    if args[1] not in works.keys():
+        raise Exception('첫번째 전달인자가 이상함 >> ' + str(works.keys()))
+
+    if args[2] not in works[args[1]].keys():
+        raise Exception('두번째 전달인자가 이상함 >> ' + str(works[args[1]].keys()))
+
+    work()
+
+  
--- Data Engineering/corona_etl/tests/extractor_test.py
+++ Data Engineering/corona_etl/tests/extractor_test.py
@@ -1,11 +1,15 @@
 import unittest
 from datajob.etl.extract.corona_api import CoronaApiExtractor
-
+from datajob.etl.extract.corona_vaccine import CoronaVaccineExtractor
 class MTest(unittest.TestCase):
 
     def test1(self):
-        CoronaApiExtractor.__extract_data()
+        CoronaApiExtractor.extract_data()
+
+    def test2(self):
+        CoronaVaccineExtractor.extract_data()
+
+
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
     unittest.main()
