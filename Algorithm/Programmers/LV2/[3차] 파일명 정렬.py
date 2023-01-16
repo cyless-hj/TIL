f4b0931f8d3f9e605f0fa796f896fea2810f936e
cyless-hj <jhj6740@gmail.com>
2023-01-16 22:26:18 +0900

    Add Algorithm Solve - Programmers LV2 23/01/16

--- Algorithm/Programmers/LV2/[3차] 파일명 정렬.py
+++ Algorithm/Programmers/LV2/[3차] 파일명 정렬.py
@@ -19,3 +19,19 @@ def solution(files):
 
     answer = sl_list
     return answer
+
+def solution(files):
+    answer = []
+
+    for i in range(len(files)):
+        tmp_idx = []
+        for j in range(len(files[i])):
+            if files[i][j].isdigit():
+                tmp_idx.append(j)
+            if tmp_idx and not files[i][j].isdigit():
+                break
+        answer.append([files[i][:tmp_idx[0]], files[i][tmp_idx[0]:tmp_idx[-1] + 1], files[i][tmp_idx[-1] + 1:]])
+
+    answer.sort(key=lambda x: (x[0].lower(), int(x[1])))
+    answer = [''.join(x) for x in answer]
+    return answer
