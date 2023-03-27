e74d1f32077bd9321245634356195a16ae8b600b
cyless-hj <jhj6740@gmail.com>
2023-03-27 20:48:32 +0900

    Add Algorithm Solve - Programmers LV3 23/03/27

---
+++ Algorithm/Programmers/LV3/징검다리 건너기.py
@@ -0,0 +1,20 @@
+def solution(stones, k):
+    answer = 0
+    start, end = 0, max(stones)
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        for stone in stones:
+            if cnt < k:
+                if stone - mid <= 0:
+                    cnt += 1
+                else:
+                    cnt = 0
+            else:
+                    break
+        if cnt < k:
+            start = mid + 1
+        else:
+            end = mid - 1
+            answer = mid
+    return answer
